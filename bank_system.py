b424506150c89aed37387a220cdb2b8d8a875361
Muhammadyusuf <mukhammadyusuf.alijanov25@gmail.com>
2021-11-22 17:34:40 +0500

    Second commit, this code working 80 percently

--- bank_system.py
+++ bank_system.py
@@ -1,5 +1,5 @@
 def print_options():
-  print(f'''
+    print('''
 1 - list accounts
 2 - transfer money
 3 - open a new account
@@ -7,44 +7,60 @@ def print_options():
     ''')
 
 def list_accounts(user):
-  print('Your accounts: ')
-  for i in range(3,len(user)):
-    print(f'{i-2}.{user[i][0]} - Balance: {user[i][1]} RUB')
+    print('Your accounts:')
+    for i in range(3, len(user)):
+        print(f'{i - 2}. {user[i][0]} - Balance: {user[i][1]} RUB')
+    
+def count_accounts(user):
+    return len(user) - 3
 
 def transfer_money(user):
-  if count_accounts(user) == 1:
-    print('You need to have at least two accounts for this operation!')
-  else:
-    list_accounts(user)
-    src_acc = request_input('Select the source account: ')
-    target_acc = request_input('Select the target account: ')
-    money_amount = request_input('Enter the amount to trandfer: ')
-    if user[3][1] > money_amount:
-      newValue = user[3][1] - money_amount
-      user[3:user] = money_amount + newValue
-      print(f'''
-      Operation succeeded!
-      Your accounts: 
-      {user}
-      ''')
-    else:
-      print('Insufficent funds on source account!')
-      transfer_money(user)
-      
+    acc_number = count_accounts(user)
+    if acc_number == 1: 
+        print('You need to have at least two accounts for this operation')
+    else: 
+        list_accounts(user)
+        src_acc, target_acc = request_accounts()
+        src_acc, target_acc = check_accounts(src_acc, target_acc, acc_number)
+        money_amount = request_input('Enter the amount to transfer: ')
+
+def request_accounts():
+        return request_input('Select the source account: '), request_input('Select the target account: ')
+
+def check_accounts(src, tgt, number):
+    while src <= 0 or src > number or tgt <= 0 or tgt > number:
+        print('You are referencing non-existing account(s)')
+        src, tgt = request_accounts()
+    
+    while tgt == src: 
+        print('You are trying to transfer money between same accounts')
+        src, tgt = request_accounts()
+    return src, tgt
+
+def check_amount():
+    pass
+
 def request_input(msg):
-  return int(input(msg))
-
-def count_accounts(user):
-      return len(user) - 3
-
-def input_credentials():
-  username_input = input('Enter username: ')
-  password_input = input('Enter password: ')
-  return username_input, password_input
+    return int(input(msg))
 
 def enter_option():
-  option = input('Choose an option: ')
-  return option
+    option = input('Choose an option: ')
+    return option
+
+def input_credentials():
+    username_input = input('Enter username: ').strip()
+    while True:
+        if username_input == '':
+            print('You have entered empty string or spaces. Please enter username again')
+            username_input = input('Enter username: ').strip()
+        else: break
+    password_input = input('Enter password: ').strip()
+    while True:
+        if password_input == '':
+            print('You have entered empty string or spaces. Please enter password again')
+            password_input = input('Enter password: ').strip()
+        else: break
+    return username_input, password_input
 
 def read_data():
   with open ('./bankdata.txt','r') as user:
@@ -56,7 +72,7 @@ def read_data():
           if account_num == 0:
               info = []
               info += dt.split(";")[0:3]
-              account_num = int(dt.split(";")[3])
+              account_num = int(dt.strip().split(";")[3])
           else:
               info.append(dt.strip().split(";"))
               account_num -= 1
@@ -64,30 +80,34 @@ def read_data():
                   output.append(info)
       return output
 
-def data_check(l, p, data):
-  for datum in data:
-    if l == datum[1] and p == datum[2]:
-      return [True, datum]
-  return [False, None]
+def check_data(l, p, data):
+    for datum in data:
+        if l == datum[1] and p == datum[2]: 
+            return [True, datum]
+    return [False, None]
 
 def logout():
-  print('Welcome to the online bank!')
-  auth()
+    print('Welcome to the online bank!')
+    auth()
 
 def auth():
-  login, password = input_credentials()
-  db = read_data()
-  authenticated, userData= data_check(login,password,db)
-  if authenticated:
-    print(f'Welcome, {userData[0]}!')
-    print_options()
-    option = enter_option()
-    if option == '1':
-      list_accounts(userData)  
-    elif option == '2':
-      transfer_money(userData)
-    elif option == '4':
-      logout()
-  else:print('Incorrect username/password!')
+    login, password = input_credentials()
+    db = read_data()
+    authenticated, userData  = check_data(login, password, db)
+    if authenticated: 
+        print(f'Welcome, {userData[0]}!')
+        print_options()
+        option = enter_option()
+        if option == '1':
+            list_accounts(userData)
+        elif option == '2':
+            transfer_money(userData)
+        elif option == '4':
+            logout()
+    else: print('Incorrect username/password!')
 
 auth()
+
+
+
+
